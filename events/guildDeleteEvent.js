b9bdf22e27c697c25a508423196e0ab292c5c38b
vaggos-thanos <56397268+vaggos-thanos@users.noreply.github.com>
2020-08-16 16:38:36 +0300

    Add files via upload

--- events/guildDeleteEvent.js
+++ events/guildDeleteEvent.js
@@ -4,22 +4,22 @@ const GuildConfig = require( '../database/schemas/GuildConfig' );
 
 module.exports = class GuildCreateEvent extends BaseEvent {
   constructor() {
-    super('guildCreate');
+    super('guildDelete');
   }
   
   async run(client, guild) {
       try {
-      const guildConfig = await GuildConfig.create({
+      const guildConfig = await GuildConfig.remove({
         guildId: guild.id,
       });
 
       // when the bor join a server send this message to the console
-      console.log(`The ( ${client.user.tag} ) has join to this server( ${guild} ). Saved to DB `);
+      console.log(`The ( ${client.user.tag} ) has left from this server ( ${guild} ). Saved to DB `);
 
       // when the bor join a server send this message to the bot's server (djvaggos)
       let myGuild = client.guilds.cache.get("744507395547332638");
       let memberCountChannel = myGuild.channels.cache.get("744515528105656331");
-      memberCountChannel.send(`The ( ${client.user.tag} ) has join to this server( ${guild} ) [ ${guild.id} ]`)
+      memberCountChannel.send(`The ( ${client.user.tag} )  has left from this server ( ${guild} ) [ ${guild.id} ]`)
 
     } catch ( err ) {
     console.log( err );
