0c8ffd3de62320c62336b0133cc64913a24070bc
vaggos-thanos <56397268+vaggos-thanos@users.noreply.github.com>
2020-08-19 13:27:56 +0300

    Delete owner.js

--- commands/owner.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: "owner",
-    description: "test command ",
-    execute(message, client) {
-        const guild = '728551838554521650';
-        message.channel.send(guild.owner); 
-    }
-};
-  
