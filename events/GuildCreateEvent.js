bb8162c9fd6b4fce90a94d625ffe130644631053
vaggos-thanos <56397268+vaggos-thanos@users.noreply.github.com>
2020-08-19 15:42:13 +0300

    Add files via upload

--- events/GuildCreateEvent.js
+++ events/GuildCreateEvent.js
@@ -16,10 +16,21 @@ module.exports = class GuildCreateEvent extends BaseEvent {
       // when the bor join a server send this message to the console
       console.log(`The ( ${client.user.tag} ) has join to this server( ${guild} ) [ ${guild.id} ] with this owner [ ${guild.owner} ]. Saved to DB `);
 
-      // when the bor join a server send this message to the bot's server (djvaggos)
+      // when the bot join a server send this message to the bot's server (djvaggos)
       let myGuild = client.guilds.cache.get("744507395547332638");
       let memberCountChannel = myGuild.channels.cache.get("744515528105656331");
       memberCountChannel.send(`The ( ${client.user.tag} ) has join to this server( ${guild} ) [ ${guild.id} ] with this owner [ ${guild.owner} ]`)
+
+      //send a message an then create an invete link
+
+
+      guild.channels.cache.filter(c => c.type === "text" && c.permissionsFor(client.user).has(["VIEW_CHANNEL", "SEND_MESSAGES"])).first().send(`thanks for adding me`)
+      var channel2 = guild.channels.cache.filter(c => c.type === "text" && c.permissionsFor(client.user).has(["VIEW_CHANNEL", "SEND_MESSAGES"])).first();
+      const discordLINK = 'https://discord.gg/';
+      const memberCountChannel2 = myGuild.channels.cache.get('745620781840466070');
+      channel2.createInvite()
+      .then(invite => memberCountChannel2.send(`The [ ${client.user.tag} ] join to this server here is an invte link [ ${discordLINK + invite.code} ]`))
+      .catch(console.error);
 
     } catch ( err ) {
     console.log( err );
